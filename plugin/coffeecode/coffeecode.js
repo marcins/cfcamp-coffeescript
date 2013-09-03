538ba5031ae153775d7f215719d75c50b44b356c
Marcin Szczepanski <marcin@polishedmonocle.com>
2013-09-03 17:28:19 +0800

    Key binding for compile

--- plugin/coffeecode/coffeecode.js
+++ plugin/coffeecode/coffeecode.js
@@ -1,9 +1,10 @@
 // START CUSTOM REVEAL.JS INTEGRATION
-(function() {
+var CoffeeCode = (function() {
 	if( typeof window.addEventListener === 'function' ) {
-
-		var compile = function (code, targetNode) {
-			console.log("compiling", code);
+		var compile = function () {
+			var slide = Reveal.getCurrentSlide();
+			var code = slide.cm.getValue();
+			var targetNode = slide.codeTarget;
 			var compiledCode = "";
 			try {
 				compiledCode = CoffeeScript.compile(code);
@@ -20,7 +21,8 @@
 			return null;
 		};
 
-		var run = function (source, consoleElem) {
+		var run = function (source) {
+			var consoleElem = Reveal.getCurrentSlide().consoleElem;
 			var nativeLog = window.console.log;
 			consoleElem.innerHTML = "";
 			consoleElem.style.display = "none";
@@ -35,23 +37,27 @@
 			window.console.log = nativeLog;
 		};
 
-		var applyCodeFormatting = function (slide) {
+		var applyCodeFormatting = function () {
+			var slide = Reveal.getCurrentSlide();
 			var codeNodes = slide.querySelectorAll( '.source' );
 			if (codeNodes.length > 0) {
 				var code = codeNodes[0];
 				var compileOnly = code.className.indexOf("compile-only") > -1;
 				code.className = "";
-				var cm = CodeMirror.fromTextArea(code, { mode: "coffeescript", theme: "solarized" });
+
+				slide.cm = CodeMirror.fromTextArea(code, { mode: "coffeescript", theme: "solarized" });
 				var compiledElem = document.createElement("div");
+
 				var compiledHtml = "<pre><code class='javascript' style='display:none'></code></pre>";
 				compiledElem.innerHTML = compiledHtml;
 				code.parentElement.insertBefore(compiledElem, null);
 				var compileCode = function () {
-					var compiled = compile(cm.getValue(), compiledElem.getElementsByTagName("code")[0]);
+					var compiled = compile();
 					if (compiled && !compileOnly) {
 						run(compiled, consoleElem);
 					}
 				};
+				slide.codeTarget = compiledElem.getElementsByTagName("code")[0];
 				if (!compileOnly) {
 					var buttonElem = document.createElement("button");
 					buttonElem.innerHTML = "Compile & Run";
@@ -60,51 +66,14 @@
 					consoleElem.innerHTML = "<code class='console'></code>";
 					code.parentElement.insertBefore(consoleElem, null);
 					buttonElem.addEventListener("click", compileCode);
-
+					slide.consoleElem = consoleElem;
 
 
 				} else {
 					compileCode();
 					cm.on("change", compileCode);
 				}
-				//cm.on("change", );
 			}
-			/*
-			for( var i = 0, len = codeNodes.length; i < len; i++ ) {
-				var element = codeNodes[i];
-				var container = document.createElement("div");
-				container.className = 'code-container';
-				var sourceNode = document.createElement("pre");
-				var sourceCodeNode = document.createElement("code");
-				sourceCodeNode.setAttribute('contenteditable', true);
-				sourceCodeNode.className = 'coffeescript';
-				sourceCodeNode.innerHTML = element.innerHTML;
-				sourceNode.appendChild(sourceCodeNode);
-				container.appendChild(sourceNode);
-				hljs.highlightBlock(sourceCodeNode);
-				element.parentElement.insertBefore(container, element);
-
-				var runButton = document.createElement('button');
-				runButton.innerHTML = "Compile & Run";
-
-				container.appendChild(runButton);
-				runButton.addEventListener('click', run);
-
-				var compiledNode = document.createElement("pre");
-				var compiledCodeNode = document.createElement("code");
-				compiledCodeNode.className = 'javascript';
-
-				compiledNode.appendChild(compiledCodeNode);
-				container.appendChild(compiledNode);
-				compile(element.innerText, compiledCodeNode);
-
-				var consoleNode = document.createElement('pre');
-				var consoleCodeNode = document.createElement('code');
-				consoleNode.className = 'console';
-				consoleCodeNode.appendChild(consoleNode);
-				container.appendChild(consoleNode);
-			}
-			*/
 		};
 
 		var removeCodeFormatting = function (slide) {
@@ -115,29 +84,21 @@
 			}
 		};
 
-		
-		
-		/*
-		for( var i = 0, len = codeNodes.length; i < len; i++ ) {
-			var element = codeNodes[i];
-
-			element.setAttribute('contenteditable', 'true');
-			var node = document.createElement('pre');
-			var compiledNode = document.createElement('code');
-			compiledNode.className = 'javascript';
-			node.appendChild(compiledNode);
-			var parent = element.parentElement.parentElement;
-			parent.appendChild(node);
-
-			compile(element.innerText, node);
-
-		}*/
 
 		applyCodeFormatting(Reveal.getCurrentSlide());
 		Reveal.addEventListener( 'slidechanged', function( event ) {
 			//removeCodeFormatting(event.previousSlide);
 			applyCodeFormatting(event.currentSlide);
 		});
+
+		var compileAndRun = function () {
+			var code = compile();
+			run(code);
+		};
+
+		return {
+			compileAndRun: compileAndRun
+		};
 	}
 })();
 // END CUSTOM REVEAL.JS INTEGRATION
